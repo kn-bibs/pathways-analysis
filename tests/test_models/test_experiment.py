2118a4df4f094496852458d0cd9e061ca0b44bfe
sienkie <sienkiewicz.karolina2@gmail.com>
2018-05-27 23:56:40 +0200

    ttest implementation

--- tests/test_models/test_experiment.py
+++ tests/test_models/test_experiment.py
@@ -1,5 +1,6 @@
 from models import Experiment, SampleCollection, Sample
 
+
 # TODO methods for creating samples and sample_collections
 
 
@@ -24,10 +25,17 @@ def test_init():
 
 
 def test_get_all():
-    pass
+    data1 = {'BAD': 1.2345, 'FUCA2': 6.5432}
+    data2 = {'BAD': 2.3456, 'FUCA2': 7.6543}
+    data3 = {'BAD': 3.4567}
 
+    tumour_samples = [Sample.from_names('Tumour_1', data1), Sample.from_names('Tumour_2', data2)]
+    normal_samples = [Sample.from_names('Normal_1', data3)]
 
-def test_fold_change():
-    pass
+    tumour = SampleCollection('Tumour', tumour_samples)
+    normal = SampleCollection('Normal', normal_samples)
 
+    experiment_samples = Experiment(case=tumour, control=normal).get_all()
 
+    assert isinstance(experiment_samples, SampleCollection)
+    assert all(label in experiment_samples.labels for label in tumour.labels + normal.labels)
